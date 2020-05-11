4d5e65947d8b84ec487923c7b33f6d4f4c1e2a61
majaalin <maja_alin@hotmail.com>
2020-05-11 16:01:45 +0200

    Adding filter on image

--- src/scripts/gameOn.js
+++ src/scripts/gameOn.js
@@ -2,12 +2,14 @@ let y = -20;
 let x = 200;
 let speed = 2;
 let score = 0;
+let opacity = 200;
 
 function gameOn() {
   background(bg);
+  pg = rect(0, 0, 800, 440);
+  fill(0, 0, 0, opacity);
   text('score = ' + score, 30, 20);
   image(garbage, x, y, 35, 35);
-  rectMode(CENTER);
   image(crab, mouseX, height - 60, 80, 60);
   y += speed;
   if (y > height) {
@@ -17,6 +19,7 @@ function gameOn() {
     y = -20;
     speed += 0.5;
     score += 1;
+    opacity -= 5;
     garbage = loadImage(
       allGarbage[Math.floor(Math.random() * allGarbage.length)]
     );
