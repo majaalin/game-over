26d8ab482416e18d5003bf3422f6f09e2697aacd
Camilla <cks@live.se>
2020-05-13 20:29:16 +0200

    updated logic for minus score
    updated garbage items and put them in a list of objects.
    moved most logic to setup instead of gameOn.

--- src/scripts/gameOn.js
+++ src/scripts/gameOn.js
@@ -6,19 +6,24 @@ let scoreText;
 let opacity = 200;
 let scoreTextColor = 250;
 
+function pickRandom() {
+  x = random(20, width - 20);
+}
+
 function gameOn() {
-  // backgroundSound.setVolume(0.01);
-  // backgroundSound.play();
+  //backgroundSound.play();
   background(bg);
+  //bg.filter(BLUR, blurred);
   fill(0, 0, 0, opacity);
   pg = rect(0, 0, 800, 440);
   textFont(scoreFont);
   fill(scoreTextColor);
   textSize(24);
-  scoreText = text('score = ' + score, 70, 35);
-  image(garbage, x, y, 35, 35);
+  scoreText = text("score = " + score, 70, 35);
+  image(garbage.image, x, y, 35, 35);
   image(crab, mouseX, height - 60, 80, 60);
   y += speed;
+
   if (y > height) {
     gameOverSound.play();
     screen = 2;
@@ -27,33 +32,20 @@ function gameOn() {
   if (y > height - 60 && x > mouseX - 50 && x < mouseX + 50) {
     scoreSound.play();
     y = -20;
-    score += 1;
+    //console.log(bg.blurred);
     opacity -= 5;
     speed += 0.2;
-    // console.log(garbage);
-    // console.log(allGarbage[0]);
 
-    // saveFrames(garbage, "png", 1, 25, (data) => {
-    //   console.log(data);
-    // });
+    if (garbage.type === "deadfish") {
+      score -= 1;
+    } else {
+      score += 1;
+    }
 
-    // if (garbage === './src/assets/deadfish.png') {
-    //   console.log('hej' + garbage);
-    //   score--;
-    // } else {
-    //   score++;
-    // }
-
-    garbage = loadImage(
-      allGarbage[Math.floor(Math.random() * allGarbage.length)]
-    );
+    garbage = allGarbage[Math.floor(Math.random() * allGarbage.length)];
   }
 
   if (y == -20) {
     pickRandom();
   }
-}
-
-function pickRandom() {
-  x = random(20, width - 20);
 }
